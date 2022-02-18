ae5e1155244c6b4a7ac8847fd6b1d65a323001e2
IgorA <Igoru.Ale@gmail.com>
2022-02-18 09:27:34 +0500

    добавлен логгер, chi Router и echo

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.13
 
 require (
 	github.com/caarlos0/env/v6 v6.9.1
+	github.com/go-chi/chi v1.5.4
 	github.com/google/uuid v1.3.0
+	github.com/rs/xid v1.3.0
+	github.com/rs/zerolog v1.26.1
 )
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"os"
 
 	"github.com/caarlos0/env/v6"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
+	"github.com/rs/xid"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
 )
 
 type config struct {
@@ -35,18 +42,34 @@ func main() {
 		panic(fmt.Sprintf("cannot parse configuration: %s", err.Error()))
 	}
 
+	logger := zerolog.New(os.Stdout).With().
+		Timestamp().
+		Str("foo", "bar").
+		Logger()
+
 	h := handler{db: db{}}
 
-	http.HandleFunc("/save", h.saveURL)
-	http.HandleFunc("/go", h.getURL)
+	// http.HandleFunc("/save", h.saveURL)
+	// http.HandleFunc("/go", h.getURL)
 
-	log.Printf("start HTTP at a port %d", cfg.Port)
-	panic(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil))
+	r := chi.NewRouter()
+	r.Use(hlog.NewHandler(logger))
 
+	r.Use(RequestIDHandler("req_id", "Request-Id"))
+	r.Use(hlog.RemoteAddrHandler("ip"))
+	r.Use(middleware)
+
+	r.Get("/save", h.saveURL)
+	r.Get("/go", h.getURL)
+	r.Get("/echo", echo)
+
+	logger.Info().Msgf("start HTTP at a port %d", cfg.Port)
+	panic(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r))
 }
 
 func (h handler) saveURL(w http.ResponseWriter, r *http.Request) {
 	// /save?url=https://www.ozon.ru
+	logger := zerolog.Ctx(r.Context())
 	v := r.URL.Query().Get("url")
 	key := uuid.New().String()
 
@@ -61,18 +84,20 @@ func (h handler) saveURL(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(msg))
 		
 		return
-	} else {
-		msg = fmt.Sprintf("короткий URL: %q", key)
-		w.Write([]byte(msg))
 	}
 
 	h.db[key] = parsedURL
-	log.Println("saveURL", key, r.URL.String())
+
+	msg = fmt.Sprintf("короткий URL: %q", key)
+	w.Write([]byte(msg))
+	logger.Info().Msgf("saveURL %s %s", key, parsedURL)
 
 }
 
 func (h handler) getURL(w http.ResponseWriter, r *http.Request) {
 	// /go?to=uuid
+	logger := zerolog.Ctx(r.Context())
+
 	key := r.URL.Query().Get("to")
 	requestedURL := h.db.Get(key)
 	if requestedURL == nil {
@@ -81,5 +106,56 @@ func (h handler) getURL(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Redirect(w, r, requestedURL.String(), http.StatusTemporaryRedirect)
-	log.Println("getURL", key, requestedURL.String())
+	logger.Info().Msgf("getURL %s %s", key, requestedURL.String())
+}
+
+func middleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		logger := zerolog.Ctx(r.Context())
+		logger.Info().Msgf("Request started, URL: %s, mehtod: %s", r.URL.String(), r.Method)
+		next.ServeHTTP(w, r)
+		logger.Info().Msg("Request finished")
+	}
+	return http.HandlerFunc(fn)
+}
+
+func RequestID(header string, r *http.Request) string {
+	id := r.Header.Get(header)
+
+	if id == "" {
+		id = xid.New().String()
+	}
+
+	return id
+}
+
+func RequestIDHandler(fieldKey, headerName string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			logger := zerolog.Ctx(ctx)
+
+			id := RequestID(headerName, r)
+
+			ctx = context.WithValue(ctx, "requestID", id)
+			r = r.WithContext(ctx)
+
+			logger.UpdateContext(func(c zerolog.Context) zerolog.Context {
+				return c.Str(fieldKey, id)
+			})
+
+			w.Header().Set(headerName, id)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	data, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		zerolog.Ctx(r.Context()).Error().Err(err).Msg("cannot dump request")
+	}
+
+	w.Write(data)
 }
